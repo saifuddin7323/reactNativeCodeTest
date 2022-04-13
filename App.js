83522b5c21e9915b60ce70cdae703bf6b2b9f49d
Vishal-Lakkapathri <vishal.lakkapathri@gmail.com>
2022-04-13 11:13:15 +0530

    RN Code Test Initial Commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {Text, View, SafeAreaView} from 'react-native';
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView>
+      <Text>React Native Code Test</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
